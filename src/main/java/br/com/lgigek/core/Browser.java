127ccd5677d7e6a8e000623bdceb93cd055e6b73
Lucas Gigek <lucasgigek@gmail.com>
2018-02-21 20:02:28 -0300

    Creation of initial methods of Browser and Elements

--- src/main/java/br/com/lgigek/core/Browser.java
+++ src/main/java/br/com/lgigek/core/Browser.java
@@ -1,5 +1,10 @@
 package br.com.lgigek.core;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,16 +13,20 @@ public class Browser {
 
 	private WebDriver driver;
 	private static Browser instance;
+	private JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
 
+	private static final Logger logger = LogManager.getLogger(Browser.class);
+	
 	public static Browser createInstance(String browserName) {
 		if (instance != null)
-			instance.driver.quit();
+			instance.closeWindow();
 		instance = new Browser(browserName);
 
 		return instance;
 	}
 
 	private Browser(String browserName) {
+		logger.info("Starting browser {}", browserName);
 		System.setProperty("webdriver.chrome.driver",
 				System.getProperty("user.dir") + "/src/test/resources/drivers/chromedriver");
 		ChromeOptions chromeOptions = new ChromeOptions();
@@ -37,14 +46,81 @@ public class Browser {
 	}
 
 	public void navigate(String url) {
+		logger.info("Navigating to url {}", url);
 		driver.get(url);
 	}
-	
+
 	public void closeTab() {
+		logger.info("Closing tab {}", driver.getTitle());
 		driver.close();
 	}
 
 	public void closeWindow() {
+		logger.info("Closing browser");
 		driver.quit();
+	}
+
+	public void refresh() {
+		logger.info("Refreshing page");
+		driver.navigate().refresh();
+	}
+
+	public void navigateBack() {
+		logger.info("Navigating back");
+		driver.navigate().back();
+	}
+
+	public void navigateFoward() {
+		logger.info("Navigating foward");
+		driver.navigate().forward();
+	}
+
+	public String getTabTitle() {
+		return driver.getTitle();
+	}
+
+	public String getTabUrl() {
+		return driver.getCurrentUrl();
+	}
+	
+	public Cookie getCookieByName(String name) {
+		return driver.manage().getCookieNamed(name);
+	}
+	
+	public Boolean verifyIfCookieExists(String name) {
+		try {
+			logger.info("Verifying if cookie named {} exists", name);
+			driver.manage().getCookieNamed(name).getValue().toString();
+			return true;
+		} catch (Exception e) {
+			logger.warn("Verification failed if cookie named {} exists", name);
+			return false;
+		}
+	}
+
+	public void clearCookies() {
+		logger.info("Clearing cookies");
+		driver.manage().deleteAllCookies();
+	}
+
+
+	public void setLocalstorageItem(String name, String value) {
+		logger.info("Setting item {} with value {} in localstorage", name, value);
+		jsExecutor.executeScript(String.format("window.localStorage.setItem('%s','%s');", name, value));
+	}
+
+	public void clearingLocalstorage() {
+		logger.info("Clearing localstorage");
+		jsExecutor.executeScript(String.format("window.localStorage.clear();"));
+	}
+	
+	public void focusFrame(By by) {
+		logger.info("Focusing frame {}", by.toString());
+		driver.switchTo().frame(driver.findElement(by));
+	}
+	
+	public void focusParentFrame() {
+		logger.info("Focusing parent frame");
+		driver.switchTo().parentFrame();
 	}
 }
--- src/main/java/br/com/lgigek/test/Element.java
+++ src/main/java/br/com/lgigek/test/Element.java
@@ -1,25 +1,139 @@
 package br.com.lgigek.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import br.com.lgigek.core.Browser;
 
 public class Element {
 
 	WebDriver driver;
-	WebElement element;
-	
+	By elementLocator;
+	private JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+
+	private static final Logger logger = LogManager.getLogger(Element.class);
+
 	public Element(By locator) {
+		elementLocator = locator;
 		Browser browser = Browser.getInstance();
-		driver = browser.getDriver(); 
-		
-		element = driver.findElement(locator);
+		driver = browser.getDriver();
+	}
+
+	public void clearValue() {
+		logger.info("Clearing {} value", elementLocator);
+		if (verifyIfElementIsPresent()) {
+			driver.findElement(elementLocator).clear();
+		}
+	}
+
+	public void click() {
+		logger.info("Clicking on {}", elementLocator);
+		if (verifyIfElementIsPresent()) {
+			driver.findElement(elementLocator).click();
+		}
+	}
+
+	public void jsExecutorClick() {
+		logger.info("Clicking with jsExecutor on {}", elementLocator);
+		if (verifyIfElementIsPresent()) {
+			jsExecutor.executeScript("arguments[0].click();", driver.findElement(elementLocator));
+		}
+	}
+
+	public String getAttribute(String name) {
+		logger.info("Getting attribute {} of {}", name, elementLocator);
+		if (verifyIfElementIsPresent()) {
+			return driver.findElement(elementLocator).getAttribute(name);
+		}
+		return null;
+	}
+
+	public String getText() {
+		logger.info("Getting text of {}", elementLocator);
+		if (verifyIfElementIsPresent()) {
+			return driver.findElement(elementLocator).getText();
+		}
+		return null;
+	}
+
+	public void type(String value) {
+		logger.info("Typing on {}", elementLocator);
+		if (verifyIfElementIsPresent()) {
+			driver.findElement(elementLocator).sendKeys(value);
+		}
+	}
+
+	public Boolean isPresent() {
+		logger.info("Verifying if {} is present", elementLocator);
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 1);
+			wait.until(ExpectedConditions.presenceOfElementLocated(elementLocator));
+			return true;
+		} catch (Exception e) {
+			logger.warn("Verification failed if element {} is present", elementLocator);
+			return false;
+		}
+	}
+
+	public Boolean isDisplayed() {
+		logger.info("Verifying if {} is displayed", elementLocator);
+		if (verifyIfElementIsPresent()) {
+			if (driver.findElement(elementLocator).isDisplayed())
+				return true;
+			else {
+				logger.warn("Verification failed if element {} is displayed", elementLocator);
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public Boolean isEnabled() {
+		logger.info("Verifying if {} is enabled", elementLocator);
+		if (verifyIfElementIsPresent())
+			if (driver.findElement(elementLocator).isEnabled())
+				return true;
+			else {
+				logger.warn("Verification failed if element {} is enabled", elementLocator);
+				return false;
+			}
+		return false;
+	}
+
+	public Boolean isSelected() {
+		logger.info("Verifying if {} is selected", elementLocator);
+		if (verifyIfElementIsPresent())
+			if (driver.findElement(elementLocator).isSelected())
+				return true;
+			else {
+				logger.warn("Verification failed if element {} is selected", elementLocator);
+				return false;
+			}
+		return false;
 	}
 	
-	public WebElement getWebElement() {
-		return element;
+	public void submit() {
+		logger.info("Submitting {}", elementLocator);
+		if (verifyIfElementIsPresent())
+			driver.findElement(elementLocator).submit();
 	}
-	
+
+	private Boolean verifyIfElementIsPresent() {
+
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.presenceOfElementLocated(elementLocator));
+			return true;
+		} catch (Exception e) {
+			logger.error("Element {} not found", elementLocator);
+			throw new RuntimeException("Element " + elementLocator + " was not present");
+		}
+
+	}
+
 }
