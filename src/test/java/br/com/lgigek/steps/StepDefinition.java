043a5618c338a9c9b569e97db94970cf6383a065
Lucas Gigek <lucasgigek@gmail.com>
2018-02-22 01:24:45 -0300

    Updating steps according to changes made in Element

--- src/test/java/br/com/lgigek/steps/StepDefinition.java
+++ src/test/java/br/com/lgigek/steps/StepDefinition.java
@@ -5,14 +5,13 @@ import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.By;
 
 import br.com.lgigek.core.Browser;
-import br.com.lgigek.test.Element;
+import br.com.lgigek.core.Element;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class StepDefinition {
 
 	Browser browser = Browser.getInstance();
-
 
 	@When("^The user navigates to Google home page$")
 	public void accessGoogleHomePage() {
@@ -21,7 +20,7 @@ public class StepDefinition {
 
 	@Then("^Google home page is displayed$")
 	public void verifyIfGoogleHomePageIsDisplayed() {
-		assertTrue(new Element(By.name("q")).getWebElement().isDisplayed());
+		assertTrue(new Element(By.name("q")).isDisplayed());
 	}
 
 	@When("^The user navigates to Facebook home page$")
@@ -31,7 +30,7 @@ public class StepDefinition {
 
 	@Then("^Facebook home page is displayed$")
 	public void verifyIfFacebookHomePageIsDisplayed() {
-		assertTrue(new Element(By.id("email")).getWebElement().isDisplayed());
+		assertTrue(new Element(By.id("email")).isDisplayed());
 	}
 
 }
