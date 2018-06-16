10cb0b24f24d7fbe90ee5f30c8b89672ccf6ae87
Lucas Gigek <lucasgigek@gmail.com>
2018-06-16 20:21:25 -0300

    Finished ScenarioInfo

--- src/main/java/br/com/lgigek/scenario/Scenario.java
+++ src/main/java/br/com/lgigek/scenario/Scenario.java
@@ -1,15 +1,35 @@
 package br.com.lgigek.scenario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Scenario {
 
 	private static final Logger logger = LogManager.getLogger(Scenario.class);
+	private static List<ScenarioInfo> infoList;
 
 	public static void fail(String message) {
 		logger.error(message);
-		throw  new RuntimeException(message);
+		throw new RuntimeException(message);
+	}
+
+	public static void clearInfoList() {
+		infoList = new ArrayList<ScenarioInfo>();
+	}
+
+	public static void setInfo(ScenarioInfo info) {
+		infoList.add(info);
+	}
+
+	public static String getInfoValue(String desiredInfoName) {
+		for (ScenarioInfo info : infoList) {
+			if (info.getInfoName().equals(desiredInfoName))
+				return info.getValue();
+		}
+		return null;
 	}
 
 }
